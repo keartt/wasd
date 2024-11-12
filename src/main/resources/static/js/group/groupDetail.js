6f08872dbe30322d4b62066992e5a7702087ab66
yjlee <edbwls2@naver.com>
2024-11-12 17:58:44 +0900

    [FEAT] 그룹 상세정보 조회 기능 추가(채팅방&그룹목록)

--- src/main/resources/static/js/group/groupDetail.js
+++ src/main/resources/static/js/group/groupDetail.js
@@ -32,55 +32,21 @@ var groupDetail = {
     // 그룹 정보 조회
     async openGroupInfo() {
         this.groupId = $('.group-detail.active').data('groupid');
-        try {
-            await Promise.all([
-                // html
-                util.ajaxPromise({ url: '/main/group/view', method: 'GET', dataType: 'html' })
-                    .then(res => {
-                        // initGroupView();
-                        popupMainOpen();
-                        $('.popup-main-box').html(res);
-                    }),
-                // json
-                util.ajaxPromise({ url: `/group/${this.groupId}`, method: 'GET', dataType: 'json' })
-                    .then(data => {
-                        // 그룹 정보로 HTML 요소 업데이트
-                        $('.groupView-title-box img').attr('src', data.groupImg); // 이미지 업데이트
-                        $('.groupView-title-box h1').text(data.groupNm); // 그룹 이름 업데이트
-                        $('.groupView-info-limit-box .groupView-title').eq(0).find('span').text(`최대 ${data.maxUserCount}명`); // 최대 인원수
-                        $('.groupView-info-limit-box .groupView-title').eq(1).find('span').eq(0).text(data.startTime ? data.startTime.substring(0, 5) : ''); // 시작 시간
-                        $('.groupView-info-limit-box .groupView-title').eq(1).find('span').eq(1).text(data.startTime || data.endTime ? '~' : 'ALL TIME'); // 시간 표시
-                        $('.groupView-info-limit-box .groupView-title').eq(1).find('span').eq(2).text(data.endTime ? data.endTime.substring(0, 5) : ''); // 종료 시간
-                        $('.groupView-info-dc-box pre').text(data.groupDc); // 그룹 설명 업데이트
-                        $('.groupView-gameNm img').attr('src', `/images/gameImg/${data.gameInfo.gameId}.png`); // 게임 이미지 업데이트
-                        $('.groupView-gameNm span').text(data.gameInfo.gameNm); // 게임 이름 업데이트
 
-                        return data.gameInfo;
-                    })
-                    // .then(userGameInfo => {
-                    //     util.ajaxPromise({ url: `/gameInfo/${userGameInfo.gameId}`, method: 'GET', dataType: 'json' })
-                    //         .then(gameData => {
-                    //
-                    //
-                    //
-                    //             console.log(gameData);
-                    //         })
-                    // })
-            ]);
-        } catch (error) {
-            console.log(error);
-            util.alert('error', '그룹 정보 조회 중 문제가 발생하였습니다. 잠시 후 다시 시도하세요.', '',undefined,undefined);
-            popupMainClose();
-        }
+        // HTML 가져오기
+        var htmlData = await util.ajaxPromise({
+            url: '/main/group/view',
+            method: 'GET',
+            dataType: 'html'
+        });
+        $('.popup-main-box').html(htmlData);
 
-        popupMainOpen();
-
+        groupView.init(this.groupId);
     },
 
     // 초기화 (중복 방지)
     destroy(){
         this.groupId = null;
-        this.userGameInfo = {};
         $('#group-dtl-li').off('click', this.openGroupInfo.bind(this));
         $('#chat-submit').off('click', sendMessage);
         $('#chat-text').off('keydown');
@@ -88,7 +54,3 @@ var groupDetail = {
         disconnect();
     }
 };
-
-function initGroupDetail() {
-    groupDetail.init();
-}
--- src/main/resources/static/js/group/groupView.js
+++ src/main/resources/static/js/group/groupView.js
@@ -1,7 +1,73 @@
-function initGroupView() {
+var groupView = {
 
-}
+    groupId: null,
+    init(groupId){
+        this.destroy();
 
-function getGameAttrInfo(){
+        this.groupId = groupId; // ID 설정
+        this.setGroupInfo();    // 그룹정보 설정
 
+        this.domEvt();          // 이벤트
+    },
+    domEvt(){
+        $('.popup-main-close-btn').on('click', function(){  // 팝업 창 닫으면 내용 초기화
+            this.destroy();
+        });
+    },
+    async setGroupInfo() {
+
+        try {
+            // 그룹 정보 JSON 조회
+            var groupData = await util.ajaxPromise({
+                url: `/group/${this.groupId}`,
+                method: 'GET',
+                dataType: 'json'
+            });
+
+            // 그룹 정보로 HTML 요소 업데이트
+            $('.groupView-title-box img').attr('src', groupData.groupImg);
+            $('.groupView-title-box h1').text(groupData.groupNm);
+            $('.groupView-info-limit-box .groupView-title').eq(0).find('span').text(`최대 ${groupData.maxUserCount}명`);
+            $('.groupView-info-limit-box .groupView-title').eq(1).find('span').eq(0).text(groupData.startTime ? groupData.startTime.substring(0, 5) : '');
+            $('.groupView-info-limit-box .groupView-title').eq(1).find('span').eq(1).text(groupData.startTime || groupData.endTime ? '~' : 'ALL TIME');
+            $('.groupView-info-limit-box .groupView-title').eq(1).find('span').eq(2).text(groupData.endTime ? groupData.endTime.substring(0, 5) : '');
+            $('.groupView-info-dc-box pre').text(groupData.groupDc);
+            $('.groupView-gameNm img').attr('src', `/images/gameImg/${groupData.gameInfo.gameId}.png`);
+            $('.groupView-gameNm span').text(groupData.gameInfo.gameNm);
+
+            // 게임 정보 JSON 조회
+            var gameInfoData = await util.ajaxPromise({
+                url: `/gameInfo/${groupData.gameInfo.gameId}`,
+                method: 'GET',
+                dataType: 'json'
+            });
+
+            // 그룹 정보의 value 값 한글명으로 변환
+            $('#groupView-gameInfo').empty();
+            var addTag='';
+            for (const gameAttr in gameInfoData.info) {
+                var selectedValue = groupData.gameInfo.info[gameAttr] || '';            // 그룹장이 선택한 값
+                var selectedText = gameInfoData.info[gameAttr][selectedValue] || '';    // 선택한 값의 이름
+                addTag += `
+                    <div class="groupView-gameInfo-col-item">
+                        <label>${gameInfoData.info[gameAttr].infoNm}</label>
+                        <input type="text" value="${selectedText}" disabled>
+                    </div>
+                `;
+            }
+            $('#groupView-gameInfo').append(addTag);
+
+            popupMainOpen(); // 팝업 OPEN
+
+        } catch (error) {
+            util.alert('error', '그룹 정보 조회 중 문제가 발생하였습니다. 잠시 후 다시 시도하세요.', '',undefined,undefined);
+
+            popupMainClose();
+            this.destroy();
+        }
+    },
+    destroy(){
+        this.groupId = null;
+        $('.popup-main-close-btn').off('click');
+    }
 }
